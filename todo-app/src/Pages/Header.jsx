7eddae2649b5d8aff9ab12287a9fe42b162fba6a
itssayushhere <yayushman088@gmail.com>
2024-07-15 16:53:47 +0530

    updated

--- todo-app/src/Pages/Header.jsx
+++ todo-app/src/Pages/Header.jsx
@@ -1,29 +1,38 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
   const links = [
-    { label: 'Home', link: '/' },
-    { label: 'About', link: '/about' },
+    { label: "Home", link: "/" },
+    { label: "About", link: "/about" },
     // Add more links as needed
-    
   ];
 
   return (
-    <div className=''>
-    <div className='fixed top-5  flex items-center w-full  justify-center  '>
-      <nav className='bg-black rounded-3xl overflow-hidden'>
-        <ul className='flex items-center justify-evenly  border  rounded-3xl p-1'>
-          {links.map((item, index) => (
-            <NavLink to={item.link} activeClassName="active" key={index}> 
-                <li key={index} className='p-5 rounded-3xl hover:bg-gray-700 px-14 hover:border-neutral-200 transition duration-500'>
-                {item.label}
-              </li>
+    <div className="">
+      <div className="fixed top-5 flex items-center w-full justify-center">
+        <nav className="bg-black rounded-3xl overflow-hidden">
+          <ul className="flex items-center justify-evenly rounded-3xl p-1 gap-1">
+            {links.map((item, index) => (
+              <NavLink
+                to={item.link}
+                className={({ isActive }) =>
+                  isActive
+                    ? " font-medium bg-gray-900 rounded-3xl border  border-gray-600 transform ease-linear duration-500"
+                    : ""
+                }
+                key={index}
+              >
+                <li
+                  key={index}
+                  className="p-4 rounded-3xl hover:bg-gray-900 px-14  transition duration-500 ease-in-out"
+                >
+                  {item.label}
+                </li>
               </NavLink>
-          ))}
-        </ul>
-      </nav>
-    </div>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
-
