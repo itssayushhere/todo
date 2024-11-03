a230f894cb5ce80a9018d3f68a424e655b1eb734
itssayushhere <yayushman088@gmail.com>
2024-11-04 01:11:05 +0530

    render link added for backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:3000/api";
 export const token = sessionStorage.getItem('token');
-export const BASE_URL = "https://different-todo-production.up.railway.app/api"
+export const BASE_URL = "https://todo-x57i.onrender.com/api"
